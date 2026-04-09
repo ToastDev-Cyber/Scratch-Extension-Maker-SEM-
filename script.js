a9d2eb639445d1bdebf05b8c251a59f0f3338e16
TheSnakeCell <toastisyummby@gmail.com>
2026-04-10 00:16:21 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,61 +1,40 @@
-// 1. Define custom "Scratch-style" Extension block
-Blockly.Blocks['ext_definition'] = {
+// Define custom Browser/Technical blocks
+Blockly.Blocks['browser_alert'] = {
   init: function() {
-    this.appendDummyInput()
-        .appendField("Extension Name:")
-        .appendField(new Blockly.FieldTextInput("My App"), "NAME");
-    this.appendStatementInput("BLOCKS")
-        .setCheck(null)
-        .appendField("Blocks");
-    this.setColour(290);
-    this.setTooltip("Define your Scratch extension here");
+    this.appendValueInput("TEXT")
+        .setCheck("String")
+        .appendField("browser alert");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour("#00BFFF");
   }
 };
 
-// 2. Initialize Workspace
+Blockly.Blocks['time_get_now'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("current timestamp");
+    this.setOutput(true, "Number");
+    this.setColour("#0FBD8C");
+  }
+};
+
+Blockly.Blocks['technical_eval'] = {
+  init: function() {
+    this.appendValueInput("JS")
+        .setCheck("String")
+        .appendField("run raw javascript");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour("#575E75");
+  }
+};
+
+// Initialize the Workspace with the new Toolbox
 const workspace = Blockly.inject('blocklyDiv', {
     toolbox: document.getElementById('toolbox'),
-    renderer: 'zelos', // This makes it look like Scratch!
+    renderer: 'zelos',
     theme: Blockly.Themes.Classic,
-    grid: { spacing: 20, length: 3, colour: '#ccc', snap: true }
+    grid: { spacing: 25, length: 3, colour: '#ccc', snap: true },
+    zoom: { controls: true, wheel: true, startScale: 0.85 }
 });
-
-// 3. Generator Logic
-function exportExtension() {
-    // Note: In a full app, you'd use Blockly.JavaScript.workspaceToCode(workspace)
-    // For this demo, we'll extract the "Name" field directly
-    const rootBlock = workspace.getAllBlocks(false).find(b => b.type === 'ext_definition');
-    
-    if (!rootBlock) {
-        alert("Please add an Extension Definition block!");
-        return;
-    }
-
-    const extName = rootBlock.getFieldValue('NAME');
-    const extId = extName.toLowerCase().replace(/[^a-z0-9]/g, '');
-
-    const code = `
-(function(Scratch) {
-  class ${extId} {
-    getInfo() {
-      return {
-        id: '${extId}',
-        name: '${extName}',
-        blocks: [{
-          opcode: 'hello',
-          blockType: Scratch.BlockType.COMMAND,
-          text: 'hello world'
-        }]
-      };
-    }
-    hello() { console.log("Hello from ${extName}!"); }
-  }
-  Scratch.extensions.register(new ${extId}());
-})(Scratch);`;
-
-    const blob = new Blob([code], {type: 'text/javascript'});
-    const a = document.createElement('a');
-    a.href = URL.createObjectURL(blob);
-    a.download = 'extension.js';
-    a.click();
-}
